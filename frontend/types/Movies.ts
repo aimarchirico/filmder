b5be8d4c0118e7e11c109f8e430681c179423095
Aimár A. Chirico <aimario2004@gmail.com>
2025-02-19 01:48:54 +0100

    refactor(types): add moviebatchsize const for easier changing
    Had to test different values because too high would cause glitching on mobile.
    
    Co-authored by: Markus

--- frontend/types/Movies.ts
+++ frontend/types/Movies.ts
@@ -1,3 +1,5 @@
+export const movieBatchSize = 20 // breaks on mobile at around 25 causing glitching
+
 export interface Genre {
   id: number;
   name: string;
@@ -11,7 +13,7 @@ export interface Movie {
 }
 
 export interface MovieCardProps {
-  currentMovie: Movie | null;
+  currentMovie?: Movie ;
   isLoading: boolean;
 }
 
