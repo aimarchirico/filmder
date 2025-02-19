ecd023f1b457ce37a929fb90cb1d9832af4540ef
Aimár A. Chirico <aimario2004@gmail.com>
2025-02-19 13:43:09 +0100

    feat(splashscreen): add splash screen for auth checks
    While checking auth shows splashscreen.
    
    Co-authored by: Helene

---
+++ frontend/components/SplashScreen.tsx
@@ -0,0 +1,15 @@
+const SplashScreen = () =>  {
+
+  return (
+    <div className="min-h-screen bg-black flex flex-col items-center justify-center">
+      <h1 className="text-6xl md:text-8xl font-bold text-secondary mb-4 animate-fade-in">
+        FILMDER
+      </h1>
+      <p className="text-xl md:text-2xl text-white opacity-75">
+        TDT4140 Group 28
+      </p>
+    </div>
+  );
+}
+
+export default SplashScreen
