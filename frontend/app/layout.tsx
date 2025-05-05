aceb6b66398e967a5bfb1f90025f5769897323ff
Aimár A. Chirico <aimar@chirico.no>
2025-05-05 13:12:28 +0200

    feat(layout): add static links in noscript for improved SEO

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* Added static links for SEO */}
+        <noscript>
+          <div>
+            <a href="/">Root</a>
+            <a href="/home">Home</a>
+            <a href="/login">Login</a>
+            <a href="/signup">Signup</a>
+            <a href="/explore">Explore</a>
+            <a href="/movies">My Movies</a>
+            <a href="/friends">Friends</a>
+            <a href="/profile">Profile</a>
+          </div>
+        </noscript>
+      </body>
     </html>
   )
 }
